5ae71c3f0c786a46260b40f5252a3c57e158e2b7
cecimoura <cecimoura91@gmail.com>
2025-03-01 16:22:44 -0300

    criado o package Lisvros e a classe Livro

--- src/livros/Livro.java
+++ src/livros/Livro.java
@@ -1,4 +1,38 @@
 package livros;
 
+
+public enum StatusLivro{
+    DISPONIVEL, EMPRESTADO
+}
+
+
 public class Livro {
+    private String titulo;
+    private String autor;
+    private StatusLivro status;
+
+    public Livro(){
+        this.status=StatusLivro.DISPONIVEL;
+    }
+
+    public Livro(String titulo, String autor){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.status=StatusLivro.DISPONIVEL;
+    }
+
+    public String getTitulo() {return titulo; }
+    public void setTitulo(String titulo) {this.titulo = titulo; }
+
+    public String getAutor() {return autor; }
+    public void setAutor(String autor) {this.autor = autor; }
+
+    public StatusLivro getStatus() {return status; }
+    public void setStatus(StatusLivro status) {this.status = status; }
+
+    public void exibirInfoLivro(){
+        System.out.println("Titulo: "+this.getTitulo());
+        System.out.println("Autor: "+this.getAutor());
+        System.out.println("Status: "+this.getStatus());
+    }
 }
