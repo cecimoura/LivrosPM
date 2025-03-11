f00fe4696fc9c46771a08de0bd35c7ff7688f820
cecimoura <cecimoura91@gmail.com>
2025-03-11 20:20:17 -0300

    adicionando comentarios

--- src/usuario/Leitor.java
+++ src/usuario/Leitor.java
@@ -2,12 +2,14 @@ package usuario;
 
 public class Leitor extends Usuario {
 
+    // Construtor da classe 'Leitor', que recebe nome e CPF e passa para o construtor da classe pai 'Usuario'
     public Leitor(String nome, String CPF) {
         super(nome, CPF); // Chama o construtor da classe pai corretamente
     }
 
+    // Sobrescrita do metodo exibirInfoUsuario da classe pai Usuario
     @Override
-    public void exibirInfoUsuario() {
+    public void exibirInfoUsuario() { // Exibe as informações do leitor/usuario
         System.out.println("Leitor(a): " + this.getNome());
         System.out.println("CPF: " + this.getCPF());
     }
